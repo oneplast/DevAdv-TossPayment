958326696228e7509fd17864a4ff17f852d5f2e0
Gyeonghun Min <riverpower6@naver.com>
2025-10-23 09:40:47 +0900

    [SETTING] TossPayment 실습 프로젝트 생성

---
+++ src/main/java/org/example/tosspayment/TossPaymentApplication.java
@@ -0,0 +1,13 @@
+package org.example.tosspayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TossPaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tosspayment/TossPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tosspayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
